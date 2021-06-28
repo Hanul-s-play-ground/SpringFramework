5bc32c3ff039831b1fa01f990ea79cf01ef75522
hanull <dkfghg3@naver.com>
2021-06-29 01:46:41 +0900

    feat: dip문제점 해결 -  AppConfig 클래스로 역할 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,8 +4,13 @@ public class MemberServiceImpl implements MemberService{
 
     // MemberServiceImpl는 MemberRepository, MemoryMemberRepository에 의존한다.
     // ========> 이는 OCP, DIP 위반
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
